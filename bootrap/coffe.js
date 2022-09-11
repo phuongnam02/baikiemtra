e13ec0ed770df1f65ae719ca03e010ca57b4aec0
phuongnam02 <namblue619@gmail.com>
2022-09-11 16:03:20 +0700

    first commit

---
+++ bootrap/coffe.js
@@ -0,0 +1,20 @@
+ocument.write('Hello Would');
+    alert('Hello');
+    var result = confirm('I love duong');
+    if(result) {
+        alert('tao cung yeu may lam ');
+    }else{
+        alert('xin cai tuoi');
+    }
+    var key = 15;
+    for (let index = 1 ; index < key; index++){
+        for(let i = 1; i < index; i++) {
+            document.write('*');
+        }
+        document.write('<br /');
+    }
+
+
+
+
+
